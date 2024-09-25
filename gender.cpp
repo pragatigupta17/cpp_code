323d291efc538cc5901126dd3adc98f18b664309
Pragati Gupta <pragatigupta0104@gmail.com>
2024-09-25 12:59:06 +0530

    switch_hii_commit

---
+++ gender.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+char c;
+cout<<"enter any char<<"\n";
+cin>>c;
+switch(c)
+{
+case 'M':
+case 'm':
+{
+cout<<"male";
+break;
+}
+case 'F':
+case 'f':
+{
+cout<<"female";
+break;
+}
+default:
+{
+cout<<"other";
+break;
+}
+
+}
---
+++ switch_hiii.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int number, data;
+cout << "enter an number"<<"\n";
+cin>> number;
+cout<<"1 for hello...."<<"\n";
+cout<<"2 for hiiii...."<<"\n";
+cout<<"3 for heyyyy...."<<"\n";
+cin>>data;
+switch(data)
+{
+case 1:
+{
+cout<<"hello....";
+break;
+}
+case 2:{
+cout<<"hiiii....";
+break;
+}
+case 3:
+{
+cout<<"heyyy....";
+break;
+}
+default:
+{
+cout<<"BYEEE....";
+break;
+
+}
+}
+
+
+}
